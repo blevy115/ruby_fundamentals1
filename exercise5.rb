0b22d902e2deb8ba86a47057f3cd8447a31f103b
Brandon Levy <brandon.levy@mail.mcgill.ca>
2017-10-30 16:07:33 -0400

    completed 6-1

--- exercise5.rb
+++ exercise5.rb
@@ -1,11 +1,15 @@
 distance = 0
-until distance == 100
+choice = 0
+until choice == "go home"
   puts "Distance from home is currently #{distance}"
-  puts "Do you want to run or walk?"
+  puts "Do you want to run or walk?  When done enter 'go home'"
   choice = gets.chomp
   if choice == "walk"
     distance += 1
   elsif choice == "run"
     distance += 5
+  elsif choice == "go home"
+  else
+    puts "Command does not exist"
   end
 end
