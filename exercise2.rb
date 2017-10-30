e084f069940cbfe9c8a83403b980d8c78f4424aa
Brandon Levy <brandon.levy@mail.mcgill.ca>
2017-10-30 14:25:18 -0400

    completed 4 parts of exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,14 @@
+# calculate 15% tip on meal
+meal = 55
+tip = meal * 0.15
+puts tip
+
+# Combining string and integer
+combination = "42".to_i + 3.14
+puts combination
+
+#String interpolation
+puts "The result of 45628 multiplied by 7839 is #{45628*7839}"
+
+#Boolean question before typing i believe answer will be true
+puts (10 < 20 && 30 < 20) || !(10 == 11)
