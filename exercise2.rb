731d0bc2cce48b0b37f08345ee1faf6e92cdfbb9
Brandon Levy <brandon.levy@mail.mcgill.ca>
2017-10-30 14:43:45 -0400

    created exercise 3 which asks for a name

--- exercise2.rb
+++ exercise2.rb
@@ -12,3 +12,15 @@ puts "The result of 45628 multiplied by 7839 is #{45628*7839}"
 
 #Boolean question before typing i believe answer will be true
 puts (10 < 20 && 30 < 20) || !(10 == 11)
+
+#Variables
+name = "Brandon"
+greeting = "Hello #{name}! It's good to see you again."
+mission = "Your mission, should you choose to accept it..."
+puts "#{greeting} #{mission}"
+
+my_number = 115
+
+puts my_number >= 100
+puts "My number less than 100"
+puts my_number <= 100
